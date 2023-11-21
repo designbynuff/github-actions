c4d6f03fc7ea4bd53b7b5c0e964aac9c8166642e
designbynuff <info@designbynuff.com>
2023-11-20 20:49:58 -0500

    Added P5 sketch

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,13 +1,22 @@
 const canvasSketch = require('canvas-sketch');
+const p5 = require("p5");
+new p5();
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  p5: true,
+  dimensions: [400, 400],
 };
 
+// const settings = {
+//   dimensions: [2048, 2048]
+// };
+
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+  return () => {
+    background(0);
+    stroke(255);
+    noFill();
+    rect(100, 100, 200, 200);
   };
 };
 
